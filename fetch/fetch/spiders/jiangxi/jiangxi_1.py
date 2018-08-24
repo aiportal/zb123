07d69e7a400efce970dc6e403d029719b5eba647
bfbd <glave666@gmail.com>
2018-08-24 15:35:15 +0800

    fix: jiangxi/1

--- fetch/fetch/spiders/jiangxi/jiangxi_1.py
+++ fetch/fetch/spiders/jiangxi/jiangxi_1.py
@@ -9,27 +9,24 @@ import re
 class Jiangxi1Spider(scrapy.Spider):
     """
     @title: 江西省公共资源交易网
-    @href: http://ggzy.jiangxi.gov.cn/jxzbw/
+    @href: http://www.jxsggzy.cn/web/
     """
     name = 'jiangxi/1'
     alias = '江西'
-    allowed_domains = ['jiangxi.gov.cn']
+    allowed_domains = ['jxsggzy.cn']
     start_urls = [
-        ('http://ggzy.jiangxi.gov.cn/jxzbw/jyxx/{1}/{0}/MoreInfo.aspx?CategoryNum={0}'.format(k, k[:6]), v)
-        for k, v in [
-            ('002001002', '招标公告/房建及市政'),
-            ('002001005', '中标公告/房建及市政'),
-            ('002002002', '招标公告/交通工程'),
-            ('002002005', '中标公告/交通工程'),
-            ('002003001', '招标公告/水利工程'),
-            ('002003004', '中标公告/水利工程'),
-            # ('002009001', '招标公告/铁路工程'),
-            # ('002009004', '中标公告/铁路工程'),
-            ('002004001', '招标公告/政府采购'),
-            ('002004004', '中标公告/政府采购'),
-            ('002010001', '招标公告/重点工程'),
-            ('002010004', '中标公告/重点工程'),
-        ]
+        ('http://www.jxsggzy.cn/web/jyxx/002001/002001001/jyxx.html', '招标公告/市政工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002001/002001004/jyxx.html', '中标公告/市政工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002002/002002002/jyxx.html', '招标公告/交通工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002002/002002005/jyxx.html', '中标公告/交通工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002003/002003001/jyxx.html', '招标公告/水利工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002003/002003004/jyxx.html', '中标公告/水利工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002005/002005001/jyxx.html', '招标公告/重点工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002005/002005004/jyxx.html', '中标公告/重点工程'),
+        ('http://www.jxsggzy.cn/web/jyxx/002006/002006001/jyxx.html', '招标公告/政府采购'),
+        ('http://www.jxsggzy.cn/web/jyxx/002006/002006004/jyxx.html', '中标公告/政府采购'),
+        ('http://www.jxsggzy.cn/web/jyxx/002010/002010001/jyxx.html', '招标公告/医药采购'),
+        ('http://www.jxsggzy.cn/web/jyxx/002010/002010002/jyxx.html', '中标公告/医药采购'),
     ]
 
     def start_requests(self):
@@ -37,8 +34,8 @@ class Jiangxi1Spider(scrapy.Spider):
             data = dict(subject=subject)
             yield scrapy.Request(url, meta={'data': data}, dont_filter=True)
 
-    link_extractor = MetaLinkExtractor(css='#MoreInfoList1_DataGrid1 tr > td > a',
-                                       attrs_xpath={'text': './/text()', 'day': '../../td[last()]//text()'})
+    link_extractor = MetaLinkExtractor(css='div.ewb-infolist ul > li > a',
+                                       attrs_xpath={'text': './/text()', 'day': '../span//text()'})
 
     def parse(self, response):
         links = self.link_extractor.links(response)
@@ -50,18 +47,18 @@ class Jiangxi1Spider(scrapy.Spider):
     def parse_item(self, response):
         """ 解析详情页 """
         data = response.meta['data']
-        pid1 = '（\w{1,5}编号：[A-Z0-9-－# ]+）'
-        pid2 = '【.+】'
-        if response.body[:4] == b'%PDF':
-            contents = ['<a href="{}" target="_blank">招标公告</a>'.format(response.url)]
-        else:
-            body = response.css('#TDContent') or response.css('.infodetail')
-            contents = body.extract()
+        contents = response.css('div.article-info div.con').extract()
+
+        # if response.body[:4] == b'%PDF':
+        #     contents = ['<a href="{}" target="_blank">招标公告</a>'.format(response.url)]
+        # else:
+        #     body = response.css('#TDContent') or response.css('.infodetail')
+        #     contents = body.extract()
+        prefix = '^\[\w{2,15}\]'
 
         day = FieldExtractor.date(data.get('day'))
         title = data.get('title') or data.get('text')
-        title = re.sub(pid1, '', title)
-        title = re.sub(pid2, '', title)
+        title = re.sub(prefix, '', title)
         g = GatherItem.create(
             response,
             source=self.name.split('/')[0],
